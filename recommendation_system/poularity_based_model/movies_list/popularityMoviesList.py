9f9ce8a430ed106dd87b5404da75cf07347a001f
kumarm <mukesh.kumar@tekmindz.com>
2020-04-05 14:48:19 +0530

    top movies list completed

--- recommendation_system/poularity_based_model/movies_list/popularityMoviesList.py
+++ recommendation_system/poularity_based_model/movies_list/popularityMoviesList.py
@@ -6,7 +6,7 @@ Created on Sat Apr  4 20:51:05 2020
 @author: mukesh
 """
 """importing libraries"""
-import os
+
 import numpy as np
 import pandas as pd
 
@@ -22,4 +22,15 @@ print(movies_list.head())
 
 print(ratings_list.head())
 
-movies_data=pd.merge(ratings_list, movies_list,on='movieId')
+def mergeList(mainList,scondaryList,col):
+    return pd.merge(mainList,scondaryList,on=col)
+    
+
+#movies_data=pd.merge(ratings_list, movies_list,on='movieId')
+movies_data=mergeList(ratings_list, movies_list,'movieId')
+movies_data.head()
+
+ratingavg =pd.DataFrame(movies_data.groupby('title')['rating'].mean())
+ratingavg['rating_counts'] = pd.DataFrame(movies_data.groupby('title')['rating'].count())
+topSortedCountRating = ratingavg.head()
+    
